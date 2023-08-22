6d794ad360aca9b17117b136b3f13dc58b13e1c1
aon123 <ahidjon97@gmail.com>
2023-08-22 23:17:17 +0900

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 
-client = MongoClient("mongodb://plant:plant2022@43.201.136.217:27017/plant",authSource="admin")
+client = MongoClient("mongodb://plant:plant2022@localhost:27017/plant",authSource="admin")
 db = client.plant
 sensorsDB = db.sensors
 settingsDB = db.settings
